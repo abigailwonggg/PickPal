d434d4fe6bf7af53b14cf9c1e74d5d4bb90669fe
Alisha Chua <alisha_chua@dlsu.edu.ph>
2024-10-07 23:45:11 +0800

    added template for report element

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,4 +40,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation ("com.google.android.material:material:1.9.0")
+    implementation ("com.google.android.material:material:1.7.0")
+
 }
