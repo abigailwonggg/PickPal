28fcbb8512272e42bb8e70a11c5a0e00d143ed90
abigailwonggg <abigail_wong@dlsu.edu.ph>
2024-10-10 23:13:41 +0800

    input text for login functionality. implement toast for no input.

--- app/src/main/java/com/mobdeve/s17/AAAJATTERY/PickPal/LoginActivity.java
+++ app/src/main/java/com/mobdeve/s17/AAAJATTERY/PickPal/LoginActivity.java
@@ -5,16 +5,25 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.EditText;
+import android.widget.Toast;
+
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText username;
+    EditText password;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
         ImageView backButton = findViewById(R.id.img_back_button);
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
 
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -28,11 +37,20 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                startActivity(intent);
-                finish();
+                String usernameInput = username.getText().toString();
+                String passwordInput = password.getText().toString();
+
+                if (usernameInput.isEmpty() || passwordInput.isEmpty()) {
+                    Toast.makeText(LoginActivity.this, "Please enter both username and password", Toast.LENGTH_SHORT).show();
+                } else {
+
+                    Intent i = new Intent(LoginActivity.this, MainActivity.class);
+                    i.putExtra("username", usernameInput);
+                    i.putExtra("password", passwordInput);
+                    startActivity(i);
+                    finish();
+                }
             }
         });
     }
 }
-
